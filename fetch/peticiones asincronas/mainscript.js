b11762200757532d05c9f8e1ba0ec025996e0d68
Jhosua <jhosuapvll@gmail.com>
2022-04-26 16:30:57 -0500

    Se hace consumo de API REST con fetch

--- fetch/peticiones asincronas/mainscript.js
+++ fetch/peticiones asincronas/mainscript.js
@@ -1,1 +1,29 @@
-alert('hola');
+// PETICIÓN A API REST
+
+fetch('https://rickandmortyapi.com/api/character')
+    .then(data => data.json())
+    .then(data =>{
+        var getUsers = data.results;
+        console.log(getUsers);
+        var getContainerForData = document.querySelector('.personajes');
+        getUsers.forEach((user)=>{
+            var createElementP = document.createElement('p');
+            createElementP.textContent = user.name;
+            getContainerForData.append(createElementP);
+
+            var createElementImg = document.createElement('img');
+            getContainerForData.append(createElementImg);
+            createElementImg.src = user.image;
+            
+            //TRAER EPISODIOS
+            var getEpisode = user.episode;
+            getEpisode.forEach((episode)=>{
+                var createElementForEpisode = document.createElement('p');
+                createElementForEpisode.textContent = episode;
+                getContainerForData.append(createElementForEpisode);
+            });
+
+            document.querySelector('.loader').style.display = "none";
+        });
+
+    });
